e6e55aad62a8f988738d57d0dd14d489bbeb684f
André FFL <afontelonga@gmail.com>
2023-04-22 09:04:25 +0100

    added new fonts and made some changes on the height properties

--- js/flappy.js
+++ js/flappy.js
@@ -9,38 +9,40 @@ function mother(){
     this.elemm=newElement('div','mother')
     const bodym=newElement('div','theirbody')
     this.setheight= heightm=>bodym.style.height=`${heightm}px`
+    this.elemm.appendChild(bodym)
 }
 
 function father(){
     this.elemf=newElement('div','father')
     const bodyf=newElement('div','theirbody')
     this.setheight= heightf=>bodyf.style.height=`${heightf}px`
+    this.elemf.appendChild(bodyf)
 }
 
 function parents(height,spacebetween,x){
     this.elemP=newElement('div','parents')
 
-    this.parentm=new mother(true)
-    this.parentf=new father (false)
+    this.parentm=new mother()
+    this.parentf=new father ()
 
-    this.elemP.appendChild(this.parentm.elemP)
-    this.elemP.appendChild(this.parentf.elemP)
+    this.elemP.appendChild(this.parentm.elemm)
+    this.elemP.appendChild(this.parentf.elemf)
 
     this.randomspace = () => {
     const motherheight=Math.random()*(height-spacebetween)
     const fatherheight=height-motherheight-spacebetween
-    this.motherheight.setheight(motherheight)
-    this.fatherheight.setheight(fatherheight)
+    this.parentm.setheight(motherheight)
+    this.parentf.setheight(fatherheight)
     }
 
-    this.getX=()=>parseInt(this.element.style.left.split('px')[0])
-    this.setX=x=>this.element.style.left=`${x}px`
-    this.getwidth=()=>this.element.clientWidth
+    this.getX=()=>parseInt(this.elemP.style.left.split('px')[0])
+    this.setX=x=>this.elemP.style.left=`${x}px`
+    this.getwidth=()=>this.elemP.clientWidth
     this.randomspace()
     this.setX(x)
 }
 
 const b= new parents (700,200,400)
-document.querySelector('[wm-flappy]').appendChild(b.element)
+document.querySelector('[wm-flappy]').appendChild(b.elemP)
 
 
